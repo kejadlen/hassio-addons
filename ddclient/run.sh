686fcf9362122b4ab6c2e77b66468e0470dff43d
Alpha Chen <alpha@kejadlen.dev>
2019-12-09 09:20:04 -0800

    update ddclient
    Clean up templating /etc/ddclient/ddclient.conf

--- ddclient/run.sh
+++ ddclient/run.sh
@@ -4,17 +4,22 @@ set +u
 CONFIG_PATH=/data/options.json
 DDCLIENT_CONF_PATH=/etc/ddclient/ddclient.conf
 
-printf "daemon=300\n\n" > $DDCLIENT_CONF_PATH
-printf "use=web\n" >> $DDCLIENT_CONF_PATH
-printf "ssl=yes\n\n" >> $DDCLIENT_CONF_PATH
-
+declare -a HOSTS
 for host in $(jq --compact-output ".hosts[]" $CONFIG_PATH); do
-  printf "protocol=$(echo ${host} | jq --raw-output .protocol), " >> $DDCLIENT_CONF_PATH
-  printf "login=$(echo ${host} | jq --raw-output .login), " >> $DDCLIENT_CONF_PATH
-  printf "password=$(echo ${host} | jq --raw-output .password) " >> $DDCLIENT_CONF_PATH
-  printf "$(echo ${host} | jq --raw-output .host)\n" >> $DDCLIENT_CONF_PATH
+  ENTRY=$(jq --raw-output '[.protocol,.login,.password,.host] | "protocol=\(.[0]), login=\(.[1]), password=\(.[2]) \(.[3])"' <<< ${host})
+  HOSTS+=("${ENTRY}")
 done
+
+cat <<-EOF > $DDCLIENT_CONF_PATH
+daemon=300
+use=web
+ssl=yes
+
+$(printf "%s\n" "${HOSTS[@]}")
+EOF
 
 chmod 600 $DDCLIENT_CONF_PATH
 
 ddclient -foreground -verbose
+
+# vim: ft=bash
