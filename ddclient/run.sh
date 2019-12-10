1583605c81b6786bc20155cdd0cc07cd309849bd
Alpha Chen <alpha@kejadlen.dev>
2019-12-10 08:14:13 -0800

    simplify templating ddclient.conf

--- ddclient/run.sh
+++ ddclient/run.sh
@@ -4,18 +4,15 @@ set +u
 CONFIG_PATH=/data/options.json
 DDCLIENT_CONF_PATH=/etc/ddclient/ddclient.conf
 
-declare -a HOSTS
-for host in $(jq --compact-output ".hosts[]" $CONFIG_PATH); do
-  ENTRY=$(jq --raw-output '[.protocol,.login,.password,.host] | "protocol=\(.[0]), login=\(.[1]), password=\(.[2]) \(.[3])"' <<< ${host})
-  HOSTS+=("${ENTRY}")
-done
+hosts=$(jq --raw-output '.hosts[] | "protocol=\(.protocol), login=\(.login), password=\(.password) \(.host)"' < $CONFIG_PATH)
 
-cat <<-EOF > $DDCLIENT_CONF_PATH
+cat <<EOF > $DDCLIENT_CONF_PATH
 daemon=300
+
 use=web
 ssl=yes
 
-$(printf "%s\n" "${HOSTS[@]}")
+$hosts
 EOF
 
 chmod 600 $DDCLIENT_CONF_PATH
