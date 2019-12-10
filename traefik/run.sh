ae3d30a07efb0ba12f446881ed60a7755124c76e
Alpha Chen <alpha@kejadlen.dev>
2019-12-09 20:54:57 -0800

    hardcore traefik config

--- traefik/run.sh
+++ traefik/run.sh
@@ -2,6 +2,22 @@
 set -e
 
 CONFIG_PATH=/data/options.json
+TRAEFIK_DYNAMIC_CONF_PATH=/data/traefik.toml
+
+cat <<-EOF > $TRAEFIK_DYNAMIC_CONF_PATH
+[http.routers]
+  [http.routers.traefik]
+    entryPoints = ["http"]
+    rule = "Host(`traefik.kejadlen.dev`)"
+    # middlewares = ["redirect-https"]
+    service = "traefik"
+    # [http.routers.traefik.tls]
+
+[http.services]
+  [http.services.traefik.loadBalancer]
+    [[http.services.traefik.loadBalancer.servers]]
+      url = "http://traefik:8080/"
+EOF
 
 traefik
 
